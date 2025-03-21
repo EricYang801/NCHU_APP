aba0937c504befeb7524e934416d270877089459
Eric Yang <jie9481@gmail.com>
2025-03-21 20:13:53 +0800

    Initial Commit

---
+++ NCHU_APP/NCHU_APPApp.swift
@@ -0,0 +1,17 @@
+//
+//  NCHU_APPApp.swift
+//  NCHU_APP
+//
+//  Created by Eric Yang on 3/21/25.
+//
+
+import SwiftUI
+
+@main
+struct NCHU_APPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ NCHU_APPTests/NCHU_APPTests.swift
@@ -0,0 +1,17 @@
+//
+//  NCHU_APPTests.swift
+//  NCHU_APPTests
+//
+//  Created by Eric Yang on 3/21/25.
+//
+
+import Testing
+@testable import NCHU_APP
+
+struct NCHU_APPTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
